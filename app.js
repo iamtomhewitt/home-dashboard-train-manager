062f82eefe16c6896a9c0395620a75768482f507
Tom Hewitt <tommhewitt@gmail.com>
2020-01-13 23:24:10 +0000

    💻 Add skeleton routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const listEndpoints = require('express-list-endpoints');
+const request = require('request');
 
 const app = express();
 
@@ -11,6 +12,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.status(200).send({ status: '🚂 SERVER OK', version, endpoints: listEndpoints(app) });
+});
+
+app.get('/departures', (req, res) => {
+    const { stationCode } = req.body;
+    const { apiKey } = req.body;
+    const { numberOfResults } = req.body;
+    const url = `https://huxley.apphb.com/departures/${stationCode}/${numberOfResults}?accessToken=${apiKey}`;
+
+    request(url, (err, resp, body) => {
+        if (err) {
+            throw (err);
+        }
+    });
+
+
+    res.status(200).send('departures');
+});
+
+app.get('/arrivals', (req, res) => {
+    res.status(200).send('arrivals');
 });
 
 const port = 3001;
